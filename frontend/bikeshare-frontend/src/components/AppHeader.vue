39b955c509b35d2807a12d7f632be80b6e373090
DucNH <duc.nh03dev@gmail.com>
2024-10-25 10:33:15 +0700

    All screens created and completed #3

--- frontend/bikeshare-frontend/src/components/AppHeader.vue
+++ frontend/bikeshare-frontend/src/components/AppHeader.vue
@@ -3,16 +3,22 @@
         <h1 class="title">{{ title }}</h1>
     </div>
     <div>
-        <v-btn variant="outlined" class="btn" href="/stations">Station</v-btn>
+        <v-btn variant="outlined" class="btn" href="/stations">Stations</v-btn>
         <v-btn variant="outlined" class="btn" href="/trips">Trips</v-btn>
-
+    </div>
+    <div>
+        <h2 class="title">{{ route.name.substring(1) }}</h2>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { useRoute } from 'vue-router';
 
 const title = ref("Bike Share Management and Analysis")
+const route = useRoute()
+const currentScreen = ref("")
+
 </script>
 
 <style>
--- frontend/bikeshare-frontend/src/pages/Trips.vue
+++ frontend/bikeshare-frontend/src/pages/Trips.vue
@@ -1,136 +1,210 @@
 <template>
     <AppHeader></AppHeader>
 
-    <div class="col w-1">
-        <div>
-            <h3 class="title">Search</h3>
-        </div>
-        <div class="search-section">
-            <v-text-field label="Station name"></v-text-field>
-            <v-radio-group inline label="Status">
-                <v-radio label="Any" value=""></v-radio>
-                <v-radio label="Active" value="active"></v-radio>
-                <v-radio label="Closed" value="closed"></v-radio>
-            </v-radio-group>
-            <v-text-field label="Address"></v-text-field>
-            <v-radio-group inline label="Power type">
-                <v-radio label="Any" value=""></v-radio>
-                <v-radio label="Solar" value="solar"></v-radio>
-                <v-radio label="Classic" value="classic"></v-radio>
-            </v-radio-group>
-            <v-range-slider label="Foot print length"
-                v-model="length_value"
-                strict thumb-label="always" :min="0" :max="50" :step="1">
-            </v-range-slider>
-            <v-range-slider label="Foot print width"
-                v-model="width_value"
-                strict thumb-label="always" :min="0" :max="50" :step="1">
-            </v-range-slider>
-        </div>
-        <div>
-            <v-btn variant="outlined" class="btn">Search</v-btn>
-        </div>
-    </div>
+    <div class="container">
+        <div class="col w-1">
+            <div class="search-content">
+                <div>
+                    <h3 class="title">Search</h3>
+                </div>
+                <div class="search-section">
+                    <v-number-input label="Trip ID" control-variant="stacked"></v-number-input>
 
-    <div class="col w-2">
-        <div>
-            <div class="col center">
-                <v-pagination length="5"></v-pagination>
-                <v-btn variant="outlined" class="btn">Charts</v-btn>
-                <v-btn variant="outlined" class="btn">Export</v-btn>
+                    <v-text-field label="Subscriber type"></v-text-field>
+
+                    <v-number-input label="Bike ID" control-variant="stacked"></v-number-input>
+
+                    <v-text-field label="Start station"></v-text-field>
+                    
+                    <v-text-field label="End station"></v-text-field>
+                    
+                    <p>Duration (in minutes)</p>
+                    <div class="flex">
+                        <v-number-input label="" control-variant="stacked" max-width="150" :min="0"></v-number-input>
+                        <p style="margin: auto;">to</p>
+                        <v-number-input label="" control-variant="stacked" max-width="150" :min="0"></v-number-input>
+                    </div>
+
+                    <!-- Start time picker -->
+                    <p>Start session</p>
+                    
+                    <v-date-input
+                        label="Date start"
+                        prepend-icon=""
+                        v-model="date"
+                    ></v-date-input>
+                    <v-text-field
+                    v-model="time"
+                    :active="menu2"
+                    :focus="menu2"
+                    label="Time start"
+                    :readonly="time_allow"
+                    >
+                        <v-menu
+                            v-model="menu2"
+                            :close-on-content-click="false"
+                            activator="parent"
+                            transition="scale-transition"
+                        >
+                            <v-time-picker
+                                v-if="menu2"
+                                v-model="time"
+                                full-width
+                            ></v-time-picker>
+                        </v-menu>
+                    </v-text-field>
+                
+                </div>
+                <div class="center">
+                    <v-btn variant="outlined" class="btn">Search</v-btn>
+                </div>
             </div>
-            <v-data-table :headers="header" :items="trips" hide-default-footer="true">
-    
-            </v-data-table>
+        </div>
+
+        <div class="col w-2">
+            <div class="table-content">
+                <div class="table-header">
+                    <v-pagination length="5"></v-pagination>
+                    <div class="button-group">
+                        <v-btn variant="outlined" class="btn" href="/charts">Charts</v-btn>
+                        <v-btn variant="outlined" class="btn">Export</v-btn>
+                    </div>
+                </div>
+                <v-data-table-server :headers="header" :items="trips" hide-default-footer>
+                </v-data-table-server>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import AppHeader from '@/components/AppHeader.vue';
-import { ref } from 'vue';
+import { VNumberInput } from 'vuetify/labs/VNumberInput'
+import { VTimePicker } from 'vuetify/labs/VTimePicker'
+import { VDateInput } from 'vuetify/labs/VDateInput'
+import { computed, onMounted, ref, watch } from 'vue';
 
 const header = [
-    { title: 'Name', key: 'name', sortable: 'false' },
-    { title: 'Status', key: 'status', sortable: 'false' },
-    { title: 'Address', key: 'address', sortable: 'false' },
-    { title: 'City asset number', key: 'city_asset_number', sortable: 'false' },
-    { title: 'Alternate name', key: 'alternate_name', sortable: 'false' },
-    { title: 'Property type', key: 'property_type', sortable: 'false' },
-    { title: 'Number of docks', key: 'number_of_docks', sortable: 'false' },
-    { title: 'Power type', key: 'power_type', sortable: 'false' },
-    { title: 'Foot print length', key: 'foot_print_length', sortable: 'false' },
-    { title: 'Foot print width', key: 'foot_print_width', sortable: 'false' },
-    { title: 'Council district', key: 'council_district', sortable: 'false' },
-    { title: 'Modified date', key: 'modified_date', sortable: 'false' },
+    { title: 'Trip ID', key: 'trip_id', sortable: false },
+    { title: 'Subscriber type', key: 'subscriber_type', sortable: false },
+    { title: 'Bike ID', key: 'bike_id', sortable: false },
+    { title: 'Start', key: 'start_time', sortable: false },
+    { title: 'Start station', key: 'start_station_name', sortable: false },
+    { title: 'End station', key: 'end_station_name', sortable: false },
+    { title: 'Duration (minutes)', key: 'duration_minutes', sortable: false },
 ]
 
 const trips = [
     {
-        name: "Test Example",
-        status: "Active",
-        address: "243A De La Thanh",
-        city_asset_number: "4361725371",
-        alternate_name: "",
-        property_type: "nonmetered_parking",
-        number_of_docks: 12,
-        power_type: "Solar",
-        foot_print_length: 50,
-        foot_print_width: 20,
-        council_district: 2,
-        modified_date: '2021-01-04 00:00:00 UTC'
+        trip_id: 4098766,
+        subscriber_type: '24 Hour Walk Up Pass',
+        bike_id: 306,
+        start_time: '2015-03-12 22:22:59 UTC',
+        start_station_name: 'ACC - West & 12th Street',
+        end_station_name: 'ACC - West & 12th Street',
+        duration_minutes: 45
     },
     {
-        name: "Lorem Ipsum",
-        status: "Closed",
-        address: "1 Dai Co Viet",
-        city_asset_number: "4361725371",
-        power_type: "Classic",
-        foot_print_length: 34
+        trip_id: 4098766,
+        subscriber_type: 'Lorem Ipsum',
+        bike_id: 306,
+        start_time: '2015-11-12 21:45:22 UTC',
+        start_station_name: 'ACC - West & 12th Street',
+        end_station_name: 'ACC - West & 12th Street',
+        duration_minutes: 11
     },
     {
-        name: "Test Example",
-        status: "Active",
-        address: "243A De La Thanh",
-        city_asset_number: "4361725371",
-        power_type: "Solar",
-        foot_print_length: 50
-    }
+        trip_id: 4098766,
+        subscriber_type: '24 Hour Walk Up Pass',
+        bike_id: 306,
+        start_time: '2015-03-12 22:22:59 UTC',
+        start_station_name: 'ACC - West & 12th Street',
+        end_station_name: 'ACC - West & 12th Street',
+        duration_minutes: 45
+    },
+    {
+        trip_id: 4098766,
+        subscriber_type: '24 Hour Walk Up Pass',
+        bike_id: 306,
+        start_time: '2015-03-12 22:22:59 UTC',
+        start_station_name: 'ACC - West & 12th Street',
+        end_station_name: 'ACC - West & 12th Street',
+        duration_minutes: 45
+    },
 ]
 
-const length_value = ref([0, 50])
-const width_value = ref([0, 30])
-
+const date = ref(null)
+const time = ref(null)
+const menu2 = ref(false)
+const modal2 = ref(false)
+const time_allow = ref(false)
 
 </script>
 
 <style>
+    .container {
+    display: flex;
+    min-height: calc(100vh - 64px);
+    gap: 20px;
+    padding: 20px;
+    }
+
     .col {
-        display: inline-block;
+    display: flex;
+    flex-direction: column;
     }
 
     .w-1 {
-        width: 25%;
+    width: 25%;
+    background-color: #f5f5f5;
+    border-radius: 8px;
     }
-    
+
     .w-2 {
-        width: 70%;
+    width: 75%;
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    }
+
+    .search-content, .table-content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    padding: 20px;
     }
 
     .title {
-        text-align: center;
+    text-align: center;
+    margin-bottom: 20px;
     }
 
-    .center {
-        align-items: center;
+    .table-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
     }
-    
+
+    .button-group {
+    display: flex;
+    gap: 10px;
+    }
+
     .btn {
-        margin: 10px;
+    margin: 10px;
     }
 
     .search-section {
-        margin: 10px
+    margin: 10px;
+    flex-grow: 1;
     }
 
+    .center {
+    display: flex;
+    justify-content: center;
+    }
+    
+    .flex {
+        display: flex;
+    }
 </style>
