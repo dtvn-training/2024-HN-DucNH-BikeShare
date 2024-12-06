96c0425039971357ca958329ed88e29791bb95bb
DucNH <duc.nh03dev@gmail.com>
2024-12-06 16:05:08 +0700

    Config updated

--- backend/bikeshare/src/main/java/com/ducnh/bikeshare/config/GoogleCloudConfig.java
+++ backend/bikeshare/src/main/java/com/ducnh/bikeshare/config/GoogleCloudConfig.java
@@ -6,31 +6,38 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 public class GoogleCloudConfig {
-    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
-    private String credentialsJson;
-
-    @Value("${GOOGLE_CLOUD_PROJECT_ID}")
-    private String projectId;
+    @Value("${GCP_SERVICE_ACCOUNT_KEY}")
+    private String serviceAccountKey;
 
     @Bean
-    public Storage storage() throws IOException {
-        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
-                new ByteArrayInputStream(credentialsJson.getBytes())
-        ).createScoped(
-                Collections.singletonList("https://www.googleapis.com/auth/cloud-platform")
-        );
+    public GoogleCredentials credentials() throws IOException {
+        if (StringUtils.hasText(serviceAccountKey)) {
+            try (InputStream credentialsStream = new ByteArrayInputStream(serviceAccountKey.getBytes())) {
+                return GoogleCredentials.fromStream(credentialsStream)
+                        .createScoped(Arrays.asList(
+                                "https://www.googleapis.com/auth/cloud-platform",
+                                "https://www.googleapis.com/auth/cloudplatformprojects.readonly"
+                        ));
+            }
+        }
+        throw new IllegalStateException("Error");
+    }
 
+    @Bean
+    public Storage storage(GoogleCredentials credentials) throws IOException {
         return StorageOptions.newBuilder()
-                .setCredentials(googleCredentials)
-                .setProjectId(projectId)
+                .setCredentials(credentials)
                 .build()
                 .getService();
     }
