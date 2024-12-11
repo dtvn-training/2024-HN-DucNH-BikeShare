38d7dc3b0faa9630114b3fc67f8aa8ac8f9fb811
DucNH <duc.nh03dev@gmail.com>
2024-12-11 17:18:27 +0700

    Update FE URL

--- backend/bikeshare/src/main/java/com/ducnh/bikeshare/constant/Constant.java
+++ backend/bikeshare/src/main/java/com/ducnh/bikeshare/constant/Constant.java
@@ -3,7 +3,7 @@ package com.ducnh.bikeshare.constant;
 public final class Constant {
     public static final String TABLE_STATIONS = "`springboot-bq.bikeshare.stations`";
     public static final String TABLE_TRIPS = "`springboot-bq.bikeshare.trips`";
-    public static final String FRONTEND_URL = "http://localhost:3000";
+    public static final String FRONTEND_URL = "https://two024-hn-ducnh-bikeshare-1.onrender.com";
     public static final int LIMIT_NUMBER_OF_RESULTS = 400;
     public static final int ROWS_PER_SHEET = 1000000;
 }
