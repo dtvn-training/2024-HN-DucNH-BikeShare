f507d1652695001828c7638a7be439a7d5f898f1
DucNH <duc.nh03dev@gmail.com>
2024-12-11 13:27:49 +0700

    Remove credential

--- backend/bikeshare/src/main/java/com/ducnh/bikeshare/service/TableService.java
+++ backend/bikeshare/src/main/java/com/ducnh/bikeshare/service/TableService.java
@@ -17,8 +17,8 @@ import java.util.UUID;
 
 @Service
 public class TableService {
-    @Value("${GCP_SERVICE_ACCOUNT_KEY}")
-    private static String GCP_SERVICE_ACCOUNT_KEY;
+//    @Value("${GCP_SERVICE_ACCOUNT_KEY}")
+//    private static String GCP_SERVICE_ACCOUNT_KEY;
 
     private static final Logger log = LoggerFactory.getLogger(TableService.class);
 
@@ -39,16 +39,15 @@ public class TableService {
     }
 
     public static Job createJob(String query) throws IOException {
-        System.out.println(GCP_SERVICE_ACCOUNT_KEY.charAt(0));
-        GoogleCredentials credentials = GoogleCredentials.fromStream(
-                new ByteArrayInputStream(GCP_SERVICE_ACCOUNT_KEY.getBytes(StandardCharsets.UTF_8))
-        ).createScoped(Arrays.asList(
-                                "https://www.googleapis.com/auth/cloud-platform",
-                                "https://www.googleapis.com/auth/cloudplatformprojects.readonly"
-                        ));
+//        GoogleCredentials credentials = GoogleCredentials.fromStream(
+//                new ByteArrayInputStream(GCP_SERVICE_ACCOUNT_KEY.getBytes(StandardCharsets.UTF_8))
+//        ).createScoped(Arrays.asList(
+//                                "https://www.googleapis.com/auth/cloud-platform",
+//                                "https://www.googleapis.com/auth/cloudplatformprojects.readonly"
+//                        ));
 
         BigQuery bigQuery = BigQueryOptions.newBuilder()
-                .setCredentials(credentials)
+//                .setCredentials(credentials)
                 .setProjectId("springboot-bq")
                 .build()
                 .getService();
